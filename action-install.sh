16ea3eaa13f5ac69120871887c885713c26c0bee
LeaWron <152857271+LeaWron@users.noreply.github.com>
2026-06-02 11:34:49 +0800

    fix(ci): action-install 始终构建 plum 数据 (#7)
    - plum 始终通过 make -C plum 构建以产出 bin/rime-install 和 data/plum
    - 仅在 SQUIRREL_BUNDLED_RECIPES 非空时安装额外 recipe

--- action-install.sh
+++ action-install.sh
@@ -41,8 +41,8 @@ make copy-rime-binaries copy-opencc-data
 echo "SQUIRREL_BUNDLED_RECIPES=${SQUIRREL_BUNDLED_RECIPES}"
 
 git submodule update --init plum
+make -C plum
+make copy-plum-data
 if [ -n "${SQUIRREL_BUNDLED_RECIPES}" ]; then
-  make -C plum
   rime_dir=plum/output bash plum/rime-install ${SQUIRREL_BUNDLED_RECIPES}
-  make copy-plum-data
 fi
